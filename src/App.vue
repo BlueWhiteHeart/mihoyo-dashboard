12a31100fe33f09a990727445a2060d930cda990
BlueWhiteHeart <51748154+BlueWhiteHeart@users.noreply.github.com>
2021-05-30 21:11:16 +0800

    Revert "feat: 架构优化"

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
   <div>
-    <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
-    <Home  />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-// import HelloWorld from './components/HelloWorld.vue'
-import Home from './components/home/Home.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    Home,
-    // HelloWorld
+    HelloWorld
   }
 })
 </script>
 
 <style>
-/* #app {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-} */
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
+    <van-button type="primary" @click="fetch">主要按钮</van-button>
+    <van-icon name="chat-o" />
+    <h1>{{ msg }}</h1>
+    <p>
+      Recommended IDE setup:
+      <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+      +
+      <a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank">
+        Vetur
+      </a>
+      or
+      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+      (if using
+      <code>&lt;script setup&gt;</code>)
+    </p>
+
+    <p>See <code>README.md</code> for more information.</p>
+
+    <p>
+      <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
+      |
+      <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+    </p>
+
+    <button @click="count++">count is: {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
   </div>
 </template>
 
@@ -14,8 +43,8 @@ export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: {
-      // type: String,
-      // required: true
+      type: String,
+      required: true
     }
   },
   // methods: {
@@ -26,7 +55,7 @@ export default defineComponent({
   // },
   setup: () => {
     const count = ref(0)
-    const { ctx }:any = getCurrentInstance()
+    const { ctx } = getCurrentInstance()
     function fetch() {
       ctx.$http.get('/test', { a: '111' } , {}).then(() => {
         //请求成功
